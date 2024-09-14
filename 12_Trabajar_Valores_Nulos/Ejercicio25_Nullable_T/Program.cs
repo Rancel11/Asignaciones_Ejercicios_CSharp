446e04b99146a30c34f8a957d828fd9d90094272
rancel cedeno <rancelcedeno02@gmail.com>
2024-09-14 17:07:49 -0400

    otros cambios

--- 12_Trabajar_Valores_Nulos/Ejercicio25_Nullable_T/Program.cs
+++ 12_Trabajar_Valores_Nulos/Ejercicio25_Nullable_T/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int? Edad = null;
+
+        
+
+        if (Edad.HasValue)
+        {
+            Console.WriteLine(Edad.Value.ToString());
+        }
+        else
+        {
+            Console.WriteLine("Edad no asignada");
+        }
+    }
+}
+
